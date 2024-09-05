2a674dac10e0ebbc7e18b70bf0e498e2fe2082b8
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-09-06 00:30:45 +0300

    feat: firs component

---
+++ my-react-app/src/components/Card.tsx
@@ -0,0 +1,22 @@
+import React, {FC} from 'react';
+
+interface CardProps {
+    width?: string;
+    height?: string;
+    children: React.ReactChild | React.ReactNode;
+}
+
+const Card: FC<CardProps> =
+    ({
+         width,
+         height,
+         children
+    }) => {
+    return (
+        <div style={{width, height, border: '1px solid gray'}}>
+            {children}
+        </div>
+    );
+};
+
+export default Card;
