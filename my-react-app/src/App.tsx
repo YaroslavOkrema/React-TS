2c7f69a08d8559b6f51e4d2d5cd3a39af5d867a2
YaroslavOkrema <okrema.y22@fpm.dnu.edu.ua>
2024-09-07 00:02:40 +0300

    feat: practice ts

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import Card from "./components/Card";
+import React, {useEffect, useState} from 'react';
+import Card, {CardVariant} from "./components/Card";
+import UserList from "./components/UserList";
+import {ITodo, IUser} from "./types/types";
+import axios from "axios";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
 
 const App = () => {
+    const [users, setUsers] = useState<IUser[]>([]);
+    const [todo, setTodos] = useState<ITodo[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+        fetchTodos();
+    });
+
+    async function fetchUsers() {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+            setUsers(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async function fetchTodos() {
+        try {
+            const response = await axios.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10');
+            setTodos(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <div>
-          <Card width='200px' height='200px'>
-                <button>Кнопка</button>
-          </Card>
+              <Card width='200px' height='200px' variant={CardVariant.primary} onClick={(num) => console.log('click', num)}>
+                    <button>Кнопка</button>
+              </Card>
+              <List
+                  items={users}
+                  renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+              />
+            <List
+                items={todo}
+                renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+            />
         </div>
     );
 };
